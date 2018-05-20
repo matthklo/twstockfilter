218fd9aa60e72c8810256465b2456b4b727c0c86
Matt Lo <matt@mochi.com.tw>
2018-05-20 17:16:28 +0800

    query data for a single stock id

--- query.py
+++ query.py
@@ -40,11 +40,33 @@ def load_tracking_file(filename):
         raise RuntimeError('Unable to load tracking file: ' + str(filename))
 
 def show_usage():
-    print('Usage: %s [-p property] [-t threshold] [-e encoding] [-r]' % sys.argv[0])
-    print('    [-p] Property name used for filtering. Default: cdy5')
+    print('Usage: %s [-p property] [-t threshold] [-s property] [-e encoding] [-f] [-r]' % sys.argv[0])
+    print('  or')
+    print('Usage: %s [-q stock id]' % sys.argv[0])
+    print('    [-p] Property used for filtering. Default: cdy5')
     print('    [-t] Threshold value for filtering property. Default: 5.0')
+    print('    [-s] Property used for sorting in descending order. Default: property used for -p')
     print('    [-e] Encoding for output. Default: utf-8')
+    print('    [-f] Show stocks only listed in ETF. Need portfolio/portfolio.json')
     print('    [-r] Show tracking status. Need portfolio/tracking.json')
+    print('    [-q] Query for a single stock by id')
+
+def show_info_for_stock(e):
+    def safe_get(e, p, v = 0.0):
+        r = e.get(p, v)
+        if r == None:
+            return v
+        return r
+
+    print('Stock: %s (%s)' % (e['id'].encode(enc), e['name'].encode(enc)))
+    print('    price: %.2f, roe: %.2f%%\t' % (safe_get(e, 'price'), safe_get(e, 'roe')))
+    print('    pt/3/5:\t%.2f, %.2f, %.2f' % (safe_get(e, 'pt'), safe_get(e, 'pt3'), safe_get(e, 'pt5')))
+    print('    eps/3/5:\t%.2f, %.2f, %.2f' % (safe_get(e, 'eps'), safe_get(e, 'eps3'), safe_get(e, 'eps5')))
+    print('    dy/3/5:\t%.2f%%, %.2f%%, %.2f%%' % (safe_get(e, 'dy'), safe_get(e, 'dy3'), safe_get(e, 'dy5')))
+    print('    cdy/3/5:\t%.2f%%, %.2f%%, %.2f%%' % (safe_get(e, 'cdy'), safe_get(e, 'cdy3'), safe_get(e, 'cdy5')))
+    print('    ady/3/5:\t%.2f%%, %.2f%%, %.2f%%' % (safe_get(e, 'ady'), safe_get(e, 'ady3'), safe_get(e, 'ady5')))
+    print('    cd/3/5:\t%.2f, %.2f, %.2f' % (safe_get(e, 'cd'), safe_get(e, 'cd3'), safe_get(e, 'cd5')))
+    print('    sd/3/5:\t%.2f, %.2f, %.2f' % (safe_get(e, 'sd'), safe_get(e, 'sd3'), safe_get(e, 'sd5')))
 
 if __name__ == '__main__':
     # Check if GOOGLE_APPLICATION_CREDENTIALS environment variable has been set
@@ -53,16 +75,19 @@ if __name__ == '__main__':
         sys.exit(1)
 
     try:
-        pairs, remaining = getopt.getopt(sys.argv[1:], 'p:t:e:hr')
+        pairs, remaining = getopt.getopt(sys.argv[1:], 'p:t:e:hrfq:s:')
     except getopt.GetoptError as e:
         print('Error: ' + str(e), file=sys.stderr)
         show_usage()
         sys.exit(1)
 
     fprop = 'cdy5'
+    sprop = None
     fval = 5.0
     enc = 'utf-8'
     tracking = False
+    etfonly = False
+    query = None
 
     for p in pairs:
         if p[0] == '-p':
@@ -73,18 +98,41 @@ if __name__ == '__main__':
             enc = p[1]
         elif p[0] == '-r':
             tracking = True
+        elif p[0] == '-f':
+            etfonly = True
+        elif p[0] == '-q':
+            query = p[1]
+        elif p[0] == '-s':
+            sprop = p[1]
         elif p[0] == '-h':
             show_usage()
             sys.exit(0)
 
-    sdict = load_portfolio('portfolio/portfolio.json')
+    if None == sprop:
+        sprop = fprop
+
+    if etfonly:
+        sdict = load_portfolio('portfolio/portfolio.json')
     if tracking:
         td = load_tracking_file('portfolio/tracking.json')
 
     client = datastore.Client()
+
+    if None != query:
+        q = client.query(kind='tw_stock_data')
+        q.add_filter('id', '=', query)
+        found = False
+        for e in q.fetch():
+            found = True
+            show_info_for_stock(e)
+
+        if not found:
+            print('No match.')
+            sys.exit(1)
+        sys.exit(0)
+
     q = client.query(kind='tw_stock_data')
-    q.add_filter(fprop, '>', fval)
-    q.order = [ '-'+fprop ]
+    q.add_filter(fprop, '>=', fval)
     elist = []
     for e in q.fetch():
         try:
@@ -92,18 +140,28 @@ if __name__ == '__main__':
         except:
             continue
 
-        if sid in sdict:
+        if etfonly:
+            if sid in sdict:
+                elist.append(e)
+        else:
             elist.append(e)
+
+    slist = sorted(elist, key=lambda item: item.get(sprop, 0.0), reverse=True)
     
-    print('Matched Count: ' + str(len(elist)))
-    for e in elist:
+    print('Matched Count: ' + str(len(slist)) + ', Sort:' + sprop)
+    for e in slist:
         pt5 = 0.0
         if 'pt5' in e:
             pt5 = e['pt5']
 
-        print('%s %s, [dy5: %.2f%%, dy: %.2f%%] [cdy5: %.2f%%, cdy: %.2f%%] [ady5: %.2f%%, ady: %.2f%%] pr: %.2f, pt5: %.2f, ref: %d' % 
-            (e['id'].encode(enc), e['name'].encode(enc), e['dy5'], e['dy'], e['cdy5'], e['cdy'], e['ady5'], e['ady'],
-            e['price'], pt5, sdict[int(e['id'])]['refcnt']))
+        if etfonly:
+            print('%s %s,\t [dy/5: %.2f%%, %.2f%%] [cdy/5: %.2f%%, %.2f%%] pr: %.2f, pt5: %.2f, ref: %d' % 
+                (e['id'].encode(enc), e['name'].encode(enc), e['dy'], e['dy5'], e['cdy'], e['cdy5'],
+                e['price'], pt5, sdict[int(e['id'])]['refcnt']))
+        else:
+            print('%s %s,\t [dy/5: %.2f%%, %.2f%%] [cdy/5: %.2f%%, %.2f%%] pr: %.2f, pt5: %.2f' % 
+                (e['id'].encode(enc), e['name'].encode(enc), e['dy'], e['dy5'], e['cdy'], e['cdy5'],
+                e['price'], pt5))
     
     if tracking:
         print('\nTracking Items')
